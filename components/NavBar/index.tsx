2bd1af98a5fc00f0ca9e4786e90d49e1273547d7
datracka <datracka@gmail.com>
2022-12-22 22:19:40 +0100

    add mobile menu dynamic

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -1,12 +1,24 @@
 import { tw } from "twind";
-import Desktop from "@/components/NavBar/Desktop.tsx";
-import Mobile from "@/components/NavBar/Mobile.tsx";
+import Hamburger from "@/components/NavBar/Hamburger.tsx";
+import Logo from "@/components/NavBar/Logo.tsx";
+import DesktopMenu from "@/components/NavBar/DesktopMenu.tsx";
+import MobileMenu from "@/components/NavBar/MobileMenu.tsx";
 
 export default function () {
   return (
     <nav className={tw`bg-gray-800`}>
-      <Desktop />
-      <Mobile />
+      <div className={tw`mx-auto max-w-7xl px-2 sm:px-6 lg:px-8`}>
+        <div className={tw`relative flex h-32 items-center justify-between`}>
+          <Hamburger />
+          <div
+            className={tw`flex flex-1 items-center justify-center sm:justify-start`}
+          >
+            <Logo />
+            <DesktopMenu />
+          </div>
+        </div>
+      </div>
+      <MobileMenu />
     </nav>
   );
 }
