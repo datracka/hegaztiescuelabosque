6e525757b3681a559c86daac7807b1485c84efd0
Juan Carlos <jcmoscardo@gmail.com>
2023-01-26 14:01:32 +0100

    Chassroom pages

--- routes/aula-infantil.tsx
+++ routes/aula-infantil.tsx
@@ -1,10 +1,37 @@
 import NavBar from "@/islands/NavBar.tsx";
 import SectionFooter from "@/components/Shared/SectionFooter.tsx";
+import Hero from "@/components/Shared/Hero.tsx";
+import SectionContent from "@/components/InfantClassroom/SectionContent.tsx";
+import SectionSaying from "@/components/Shared/SectionSaying.tsx";
+import SectionGrid from "@/components/Shared/SectionGrid.tsx";
 
 export default function AulaInfantil() {
+  const staticPath = "/imgs/infant-classroom";
   return (
     <>
       <NavBar />
+      <Hero
+        title="INFANTIL"
+        description="CASITA DEL BOSQUE"
+        backgroundImage={`${staticPath}/hero-image.jpg`}
+      />
+      <SectionContent staticPath={staticPath} />
+      <SectionSaying
+        text="Mira profundamente en la naturaleza y entonces comprenderás todo mejor."
+        author="ALBERT EINSTEIN"
+      />
+      <SectionGrid
+        list={[
+          { alt: "Grid 1", img: `${staticPath}/grid-img-01.jpg` },
+          { alt: "Grid 2", img: `${staticPath}/grid-img-02.jpg` },
+          { alt: "Grid 3", img: `${staticPath}/grid-img-01.jpg` },
+          { alt: "Grid 4", img: `${staticPath}/grid-img-02.jpg` },
+          { alt: "Grid 5", img: `${staticPath}/grid-img-01.jpg` },
+          { alt: "Grid 6", img: `${staticPath}/grid-img-02.jpg` },
+          { alt: "Grid 7", img: `${staticPath}/grid-img-01.jpg` },
+          { alt: "Grid 8", img: `${staticPath}/grid-img-02.jpg` },
+        ]}
+      />
       <SectionFooter />
     </>
   );
