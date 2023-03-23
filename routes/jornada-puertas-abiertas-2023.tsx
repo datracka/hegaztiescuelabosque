0c5c6ac3817ef349570b447527c0ebf87ea9e7d2
datracka <datracka@gmail.com>
2023-03-23 11:03:17 +0100

    remove dates again

--- routes/jornada-puertas-abiertas-2023.tsx
+++ routes/jornada-puertas-abiertas-2023.tsx
@@ -12,14 +12,12 @@ export default function JornadaPuertasAbiertas() {
     <>
       <Head>
         <title>Hegazti Escuela Bosque | Jornada de pueetas abiertas</title>
-        // los dias 21 y 22 de Abril,
         <meta
           name="description"
           content="El próximo mes, tendremos una jornada de puertas abiertas para que puedas conocer nuestra escuela y nuestro proyecto educativo."
         />
       </Head>
       <NavBar />
-      // 21 y 22 de Abril en Viana Navarra...
       <Hero
         title="Jornada de puertas abiertas"
         description="Desarrollo, Ritmos, Vivencias, ¡Ven a vivir la experiencia!"
