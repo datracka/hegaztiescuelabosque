0483db4c86c6742e5feb8303809cba093bd1ef88
datracka <datracka@gmail.com>
2023-12-06 12:20:58 +0100

    modified autogenarted fields after compilation

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,22 +3,22 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $0 from "./routes/_app.tsx";
-
 import * as $1 from "./routes/aula-extraescolares-ludoteca.tsx";
 import * as $2 from "./routes/aula-extraescolares-sabado-en-familia.tsx";
 import * as $3 from "./routes/aula-infantil.tsx";
 import * as $4 from "./routes/aula-madre-de-dia.tsx";
 import * as $5 from "./routes/aula-primaria.tsx";
-import * as $6 from "./routes/index.tsx";
-import * as $7 from "./routes/jornada-puertas-abiertas-2023.tsx";
-import * as $8 from "./routes/legal-aviso-legal.tsx";
-import * as $9 from "./routes/legal-politica-de-cookies.tsx";
-import * as $10 from "./routes/legal-politica-de-privacidad.tsx";
-import * as $11 from "./routes/mas-informacion.tsx";
-import * as $12 from "./routes/quienes-somos-espacios.tsx";
-import * as $13 from "./routes/quienes-somos-faqs.tsx";
-import * as $14 from "./routes/quienes-somos-hegazti.tsx";
-import * as $15 from "./routes/quienes-somos-proyecto.tsx";
+import * as $6 from "./routes/extraescolares-2023-2024.tsx";
+import * as $7 from "./routes/index.tsx";
+import * as $8 from "./routes/jornada-puertas-abiertas-2023.tsx";
+import * as $9 from "./routes/legal-aviso-legal.tsx";
+import * as $10 from "./routes/legal-politica-de-cookies.tsx";
+import * as $11 from "./routes/legal-politica-de-privacidad.tsx";
+import * as $12 from "./routes/mas-informacion.tsx";
+import * as $13 from "./routes/quienes-somos-espacios.tsx";
+import * as $14 from "./routes/quienes-somos-faqs.tsx";
+import * as $15 from "./routes/quienes-somos-hegazti.tsx";
+import * as $16 from "./routes/quienes-somos-proyecto.tsx";
 import * as $$0 from "./islands/AnimatedMenuItem.tsx";
 import * as $$1 from "./islands/AnimatedMobileMenuItem.tsx";
 import * as $$2 from "./islands/CookieBanner.tsx";
@@ -34,22 +34,22 @@ import * as $$10 from "./islands/SectionMasInformacion.tsx";
 const manifest = {
   routes: {
     "./routes/_app.tsx": $0,
-
     "./routes/aula-extraescolares-ludoteca.tsx": $1,
     "./routes/aula-extraescolares-sabado-en-familia.tsx": $2,
     "./routes/aula-infantil.tsx": $3,
     "./routes/aula-madre-de-dia.tsx": $4,
     "./routes/aula-primaria.tsx": $5,
-    "./routes/index.tsx": $6,
-    "./routes/jornada-puertas-abiertas-2023.tsx": $7,
-    "./routes/legal-aviso-legal.tsx": $8,
-    "./routes/legal-politica-de-cookies.tsx": $9,
-    "./routes/legal-politica-de-privacidad.tsx": $10,
-    "./routes/mas-informacion.tsx": $11,
-    "./routes/quienes-somos-espacios.tsx": $12,
-    "./routes/quienes-somos-faqs.tsx": $13,
-    "./routes/quienes-somos-hegazti.tsx": $14,
-    "./routes/quienes-somos-proyecto.tsx": $15,
+    "./routes/extraescolares-2023-2024.tsx": $6,
+    "./routes/index.tsx": $7,
+    "./routes/jornada-puertas-abiertas-2023.tsx": $8,
+    "./routes/legal-aviso-legal.tsx": $9,
+    "./routes/legal-politica-de-cookies.tsx": $10,
+    "./routes/legal-politica-de-privacidad.tsx": $11,
+    "./routes/mas-informacion.tsx": $12,
+    "./routes/quienes-somos-espacios.tsx": $13,
+    "./routes/quienes-somos-faqs.tsx": $14,
+    "./routes/quienes-somos-hegazti.tsx": $15,
+    "./routes/quienes-somos-proyecto.tsx": $16,
   },
   islands: {
     "./islands/AnimatedMenuItem.tsx": $$0,
